ebd70a492f4a496457cfa52276cbb6e163f8a1fa
Mengxiao Lin <linmx0130@gmail.com>
2025-04-25 23:48:32 -0700

    stdio/stdout pipe demo

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
 use types::{MCPServerCommand, NahError};
+use std::process::{Command, Stdio};
+use std::io::{Read, Write, BufRead};
+use std::cell::Ref;
 
 /// Read some lines of a file
 #[derive(Debug, Parser)]
@@ -66,6 +69,37 @@ fn load_mcp_servers_config(path: PathBuf) -> Result<HashMap<String, MCPServerCom
 fn main() {
     let args = Cli::parse();
     println!("Config file: {:?}", args.mcp_config_file);
-    let data = load_mcp_servers_config(args.mcp_config_file);
-    println!("{:?}", data);
+    let data = load_mcp_servers_config(args.mcp_config_file).unwrap();
+    println!("Found servers:");
+    for server in data.keys() {
+        println!(" - {}", server);
+    }
+    
+    let mut server_processes : HashMap<String, std::process::Child> = HashMap::new();
+    for (server_name, command) in data.iter() {
+        println!("Launching server: {}", server_name);
+        let mut server_command = Command::new(&command.command);
+        for arg in command.args.iter() {
+            server_command.arg(&arg);
+        }
+        server_command.stdin(Stdio::piped());
+        server_command.stdout(Stdio::piped());
+        // server_command.stdout(Stdio::inherit());
+
+        let mut server_process = server_command.spawn().unwrap();
+        let mut child_stdin = server_process.stdin.take().unwrap();
+        child_stdin.write_all(b"{\"jsonrpc\":\"2.0\",\"method\":\"initialize\",\"id\":1, \"params\":{\"protocolVersion\":\"2024-11-05\",\"capabilities\":{},\"clientInfo\":{\"name\":\"nah\",\"version\":\"0.1\"}}}\n").unwrap();
+        child_stdin.flush().unwrap();
+
+        let mut child_stdout = server_process.stdout.take().unwrap();
+        let mut child_stdout_reader = BufReader::new(child_stdout);
+        let mut buf = String::new();
+        println!("Waiting for result");
+        child_stdout_reader.read_line(&mut buf).unwrap();
+        println!("{:?}", buf);
+        
+        println!("Termiante the server...");
+        server_process.kill();
+    }
+
 }
